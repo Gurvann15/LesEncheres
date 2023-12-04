502a5dfb857324ee93b33caaddb211a6ff186c72
Gwen <depoorter.gwenaelle@gmail.com>
2023-12-04 20:55:41 +0100

    travail sur les classes

--- src/main/java/fr/eni/lesencheres/bo/Article.java
+++ src/main/java/fr/eni/lesencheres/bo/Article.java
@@ -1,5 +1,35 @@
 package fr.eni.lesencheres.bo;
 
-public class Article {
+import java.time.LocalDate;
 
+// Table liée en BDD : articles_vendus
+public class Article {
+	private int numArticle; // no_article / Clé primaire
+	private String nomArticle; //nom_article / TO DO: vérifier pas + 30 caractères en DLL
+	private String description; // drescription / TO DO: vérifier pas + 300 caractères en DLL
+	private LocalDate dateDebutEnchere; //date_debut_encheres 
+	private LocalDate dateFinEnchere; // date_fin_encheres
+	private int prixInitial; // prix_initial / valeur par défault : nulle
+	private int prixVente; // prix_vente  / valeur par défault : nulle
+	private Utilisateur utilisateurAcheteur;
+	private Utilisateur utilisateurVendeur;
+	private Categorie categorie;
+	
+	
+	
+	
+	
+	/*
+
+  `prix_initial` int DEFAULT NULL,
+  `prix_vente` int DEFAULT NULL,
+  `no_utilisateur` int NOT NULL,
+  `no_categorie` int NOT NULL,
+  
+  KEY `FK_articles_vendus_categories` (`no_categorie`),
+  KEY `FK_articles_vendus_utilisateurs_2` (`no_utilisateur`),
+  CONSTRAINT `FK_articles_vendus_categories` FOREIGN KEY (`no_categorie`) REFERENCES `categories` (`no_categorie`),
+  CONSTRAINT `FK_articles_vendus_utilisateurs` FOREIGN KEY (`no_utilisateur`) REFERENCES `utilisateurs` (`no_utilisateur`),
+  CONSTRAINT `FK_articles_vendus_utilisateurs_2` FOREIGN KEY (`no_utilisateur`) REFERENCES `utilisateurs` (`no_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;*/
 }
--- src/main/java/fr/eni/lesencheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/lesencheres/bo/Utilisateur.java
@@ -7,16 +7,16 @@ import java.util.List;
 public class Utilisateur {
 
 	// ATTRIBUTS
-	private int numUtilisateur; // clé primaire / noUtlisateur
+	private int numUtilisateur; // no_utilisateur / clé primaire
 	private String pseudo; // pseudo / TO DO: vérifier pas + 30 caractères en DLL
 	private String nom; // nom / TO DO: vérifier pas + 30 caractères en DLL
 	private String prenom; // prenom / TO DO: vérifier pas + 30 caractères en DLL
 	private String email; // email / TO DO: vérifier pas + 20 caractères en DLL + regex
 	private String telephone; // telephone / TO DO: vérifier pas + 15 caractères en DLL + regex
 	private String rue; // rue / TO DO: vérifier pas + 30 caractères en DLL
-	private String codePostal; // codePostal / TO DO: vérifier pas + 10 caractères en DLL + regex
+	private String codePostal; // code_postal / TO DO: vérifier pas + 10 caractères en DLL + regex
 	private String ville; // ville / TO DO: vérifier pas + 30 caractères en DLL
-	private String password; // motDePasse / TO DO: vérifier pas + 30 caractères en DLL
+	private String password; // mot_de_passe / TO DO: vérifier pas + 30 caractères en DLL
 	private int credit; // credit
 	private boolean administrateur; // administrateur
 	private List<Article> listeArticleAchat;
